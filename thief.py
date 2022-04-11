11256d39f4342d524b3d3fed0e3175f92b935a6b
nvanslyke <nvs463@gmail.com>
2022-04-11 18:59:54 -0500

    text works better

--- thief.py
+++ thief.py
@@ -19,7 +19,7 @@ def main():
 def send_text(file):
     try:
         yag = yagmail.SMTP(user='throwaway3123456@gmail.com', password='throwAway!123')
-        yag.send(to='5128155645@txt.att.net', subject='Unauthorized user detected', contents=file)
+        yag.send(to='5128155645@mms.att.net', subject='Unauthorized user detected', contents=file)
         print("Email sent successfully")
     except:
         print("Error, email was not sent")
@@ -28,3 +28,4 @@ def send_text(file):
 
 if __name__ == "__main__":
     main()
+

1aeb2abd7d9e6aeb13b59e66db894cb8690ddc2a
nvanslyke <nvs463@gmail.com>
2022-04-11 17:42:02 -0500

    open cv live video --- maybe doesn't work

--- thief.py
+++ thief.py
@@ -4,6 +4,18 @@ import yagmail.oauth2
 #import face-recognition
 
 
+
+def main():
+
+    video = cv.VideoCapture()
+    video.open(0, cv.CAP_DSHOW)
+    while True:
+        ret, frame = video.read()
+        cv.imshow('cam', frame)
+
+
+
+
 def send_text(file):
     try:
         yag = yagmail.SMTP(user='throwaway3123456@gmail.com', password='throwAway!123')
@@ -13,5 +25,6 @@ def send_text(file):
         print("Error, email was not sent")
 
 
-send_text("file")
 
+if __name__ == "__main__":
+    main()

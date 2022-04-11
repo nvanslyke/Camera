9685ca7619536b61de31caeae86cb256ce979898
nvanslyke <nvs463@gmail.com>
2022-04-11 17:23:08 -0500

    try catch

--- thief.py
+++ thief.py
@@ -4,16 +4,14 @@ import yagmail.oauth2
 #import face-recognition
 
 
-def send_text():
+def send_text(file):
+    try:
+        yag = yagmail.SMTP(user='throwaway3123456@gmail.com', password='throwAway!123')
+        yag.send(to='5128155645@txt.att.net', subject='Unauthorized user detected', contents=file)
+        print("Email sent successfully")
+    except:
+        print("Error, email was not sent")
 
-    yag = yagmail.SMTP(user='throwaway3123456@gmail.com', password='throwAway!123')
-    yag.send(to='5128155645@txt.att.net', subject='Testing Yagmail', contents='Hurray, it worked!')
-    print("Email sent successfully")
 
-    print("Error, email was not sent")
+send_text("file")
 
-
-send_text()
-
-print("hello world")
-print("goodbye world")

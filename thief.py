fa371623dd046dcb8e699524a3bfcdf57599a54a
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2022-04-11 20:09:57 -0500

    Changed the import statement for face_recognition to be the correct one. Also changed try: except to try: finally: because we're handling all exceptions in just one way.

--- thief.py
+++ thief.py
@@ -1,8 +1,7 @@
 import cv2 as cv
 import yagmail
 import yagmail.oauth2
-#import face-recognition
-
+import face_recognition
 
 
 def main():
@@ -14,16 +13,13 @@ def main():
         cv.imshow('cam', frame)
 
 
-
-
 def send_text(file):
     try:
         yag = yagmail.SMTP(user='throwaway3123456@gmail.com', password='throwAway!123')
         yag.send(to='5128155645@mms.att.net', subject='Unauthorized user detected', contents=file)
         print("Email sent successfully")
-    except:
+    finally:
         print("Error, email was not sent")
-
 
 
 if __name__ == "__main__":

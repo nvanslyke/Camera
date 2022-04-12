8a0dcaaa64731c2e7fd336f3d83fd7abb901895a
nvanslyke <nvs463@gmail.com>
2022-04-11 21:21:37 -0500

    more stuff

--- thief.py
+++ thief.py
@@ -1,16 +1,36 @@
 import cv2 as cv
 import yagmail
 import yagmail.oauth2
+import os
+from PIL import Image
+import PIL
+import time
 import numpy as np
-#import face_recognition
+
 
 
 def main():
-
+    path_to_photo = "C:\\Users\\nvans\\Desktop\\facePhoto\\"
     video = cv.VideoCapture()
     video.open(0, cv.CAP_DSHOW)
     while True:
         useless, frame = video.read()
+        grey = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        faceCascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+        faces = faceCascade.detectMultiScale(
+            grey,
+            scaleFactor=1.3,
+            minNeighbors=3,
+            minSize=(30, 30)
+        )
+
+        if len(faces) > 0:
+
+            cv.imwrite("UnknownUser.jpg", frame)
+
+            send_text()
+
         cv.imshow('cam', frame)
         if cv.waitKey(1) == ord('q'):
             break
@@ -18,13 +38,15 @@ def main():
     video.release()
     cv.destroyAllWindows()
 
-def send_text(file):
+def send_text():
     email = "throwaway3123456@gmail.com"
     password = "fgzpxpcvkycqhsye"
 
+    UnknownUser = ["<img src='C:\\Users\\nvans\\Desktop\\Camera\\UnknownUser'>"]
+
     try:
         yag = yagmail.SMTP(user=email, password=password)
-        yag.send(to='5128155645@mms.att.net', subject='Unauthorized User Detected', contents=file)
+        yag.send(to='5128155645@mms.att.net', subject='Unauthorized User Detected', contents=UnknownUser)
         print("Email sent successfully")
     finally:
         print("Error, email was not sent")

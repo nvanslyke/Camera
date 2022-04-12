63d40173f8a6df1124c4fcd46d8696ae57b41a40
nvanslyke <nvs463@gmail.com>
2022-04-11 20:47:44 -0500

    stuff

--- thief.py
+++ thief.py
@@ -1,7 +1,8 @@
 import cv2 as cv
 import yagmail
 import yagmail.oauth2
-import face_recognition
+import numpy as np
+#import face_recognition
 
 
 def main():
@@ -9,14 +10,21 @@ def main():
     video = cv.VideoCapture()
     video.open(0, cv.CAP_DSHOW)
     while True:
-        ret, frame = video.read()
+        useless, frame = video.read()
         cv.imshow('cam', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
 
+    video.release()
+    cv.destroyAllWindows()
 
 def send_text(file):
+    email = "throwaway3123456@gmail.com"
+    password = "fgzpxpcvkycqhsye"
+
     try:
-        yag = yagmail.SMTP(user='throwaway3123456@gmail.com', password='throwAway!123')
-        yag.send(to='5128155645@mms.att.net', subject='Unauthorized user detected', contents=file)
+        yag = yagmail.SMTP(user=email, password=password)
+        yag.send(to='5128155645@mms.att.net', subject='Unauthorized User Detected', contents=file)
         print("Email sent successfully")
     finally:
         print("Error, email was not sent")
@@ -24,4 +32,5 @@ def send_text(file):
 
 if __name__ == "__main__":
     main()
+
 

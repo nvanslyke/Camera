66109982dbeccfee80fb827c423607e7eb9e77e4
nvanslyke <95662921+nvanslyke@users.noreply.github.com>
2022-04-16 22:12:48 -0500

    new

---
+++ thief2.0.py
@@ -0,0 +1,67 @@
+import face_recognition
+import numpy
+import cv2 as cv
+import platform
+import os
+
+import sendtext
+
+red = (0, 0, 255)
+green = (0, 255, 0)
+
+def main():
+
+    nathan = face_recognition.load_image_file("nathan.jpg")
+    nathan_encoding = face_recognition.face_encodings(nathan)[0]
+
+    authorized_encodings = [nathan_encoding]
+
+    video = cv.VideoCapture()
+
+    if platform.system() == "Windows":
+        video.open(0, cv.CAP_DSHOW)
+    else:
+        video.open(0, cv.CAP_V4L)
+
+
+    while True:
+
+        useless, frame = video.read()
+
+
+        face_locations = face_recognition.face_locations(frame)
+        face_encodings = face_recognition.face_encodings(frame, face_locations)
+
+        breaker = False
+        for (x, y, w, h), face_encoding in zip(face_locations, face_encodings):
+
+            results = face_recognition.compare_faces(authorized_encodings, face_encoding)
+            print(results)
+
+            if results[0]:
+                cv.rectangle(frame, (h, x), (y, w), green, 2)
+            else:
+                cv.rectangle(frame, (h, x), (y, w), red, 2)
+                cv.imwrite("UnknownUser.jpg", frame)
+                sendtext.send_text("UnknownUser.jpg")
+                os.remove("UnknownUser.jpg")
+                breaker = True
+
+
+            if breaker:
+                break
+
+        if breaker:
+            break
+
+
+        cv.imshow('pic', frame)
+
+        if cv.waitKey(1) == ord('q'):
+            break
+
+    video.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()

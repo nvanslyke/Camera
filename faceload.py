555ccf7e3d33b0ce9adc7e60a2b56c89e4c3bf3c
Aaroh Gokhale <gokhaleaaroh@gmail.com>
2022-04-19 21:24:06 -0500

    Preloading faces
    Added preloading of faces

---
+++ faceload.py
@@ -0,0 +1,17 @@
+import face_recognition
+import pickle
+
+all_face_encodings = {}
+
+nathan = face_recognition.load_image_file("nathan.JPG")
+nathan_encoding = face_recognition.face_encodings(nathan)[0]
+
+aaroh = face_recognition.load_image_file("aaroh.jpg")
+aaroh_encoding = face_recognition.face_encodings(aaroh)[0]
+
+print("Your Mom")
+
+all_face_encodings = [nathan_encoding, aaroh_encoding]
+
+with open('dataset_faces.dat', 'wb') as f:
+    pickle.dump(all_face_encodings, f)
